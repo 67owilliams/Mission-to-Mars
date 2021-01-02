ba075c8288d42c79f971b5850e241779010ff2e1
67owilliams <67owilliams@gmail.com>
2021-01-01 18:05:26 -0600

    Adding index.html

--- Scraping.py
+++ Scraping.py
@@ -1,21 +1,17 @@
 # Import Splinter, BeautifulSoup, and Pandas
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
-from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
 import datetime as dt
 
-# Set the executable path and initialize the chrome browser in splinter
-#executable_path = {'executable_path': 'chromedriver'}
-#executable_path = {'executable_path': ChromeDriverManager().install()}
-#browser = Browser('chrome', **executable_path)
 
 def scrape_all():
     # Initiate headless driver for deployment
     browser = Browser("chrome", executable_path="chromedriver", headless=True)
+
     news_title, news_paragraph = mars_news(browser)
 
-    # Run all scraping functions and store results in dictionary
+    # Run all scraping functions and store results in a dictionary
     data = {
         "news_title": news_title,
         "news_paragraph": news_paragraph,
@@ -28,7 +24,9 @@ def scrape_all():
     browser.quit()
     return data
 
+
 def mars_news(browser):
+
     # Scrape Mars News
     # Visit the mars nasa news site
     url = 'https://mars.nasa.gov/news/'
@@ -43,23 +41,17 @@ def mars_news(browser):
 
     # Add try/except for error handling
     try:
-        slide_elem = news_soup.select_one('ul.item_list li.slide')
-        slide_elem.find("div", class_='content_title')
-
-        # Use the parent element to find the first `a` tag and save it as `news_title`
-        news_title = slide_elem.find("div", class_='content_title').get_text()
-        #news_title
-
+        slide_elem = news_soup.select_one("ul.item_list li.slide")
+        # Use the parent element to find the first 'a' tag and save it as 'news_title'
+        news_title = slide_elem.find("div", class_="content_title").get_text()
         # Use the parent element to find the paragraph text
-        news_p = slide_elem.find('div', class_="article_teaser_body").get_text()
-        #news_p
-    
+        news_p = slide_elem.find("div", class_="article_teaser_body").get_text()
+
     except AttributeError:
         return None, None
 
     return news_title, news_p
 
-#Featured Images
 
 def featured_image(browser):
     # Visit URL
@@ -67,7 +59,7 @@ def featured_image(browser):
     browser.visit(url)
 
     # Find and click the full image button
-    full_image_elem = browser.find_by_id('full_image')
+    full_image_elem = browser.find_by_id('full_image')[0]
     full_image_elem.click()
 
     # Find the more info button and click that
@@ -78,40 +70,37 @@ def featured_image(browser):
     # Parse the resulting html with soup
     html = browser.html
     img_soup = soup(html, 'html.parser')
+
+    # Add try/except for error handling
     try:
         # Find the relative image url
         img_url_rel = img_soup.select_one('figure.lede a img').get("src")
-        #img_url_rel
 
     except AttributeError:
         return None
 
-    # Use the base URL to create an absolute URL
+    # Use the base url to create an absolute url
     img_url = f'https://www.jpl.nasa.gov{img_url_rel}'
-    #img_url
 
     return img_url
-
-#Mars Facts
 
 def mars_facts():
     # Add try/except for error handling
     try:
-        # use 'read_html" to scrape the facts table into a dataframe
+        # Use 'read_html' to scrape the facts table into a dataframe
         df = pd.read_html('http://space-facts.com/mars/')[0]
-    
+
     except BaseException:
-      return None
-    
+        return None
+
     # Assign columns and set index of dataframe
-    df.columns=['description', 'value']
-    df.set_index('description', inplace=True)
-    #df
-    
+    df.columns=['Description', 'Mars']
+    df.set_index('Description', inplace=True)
+
     # Convert dataframe into HTML format, add bootstrap
-    return df.to_html()
+    return df.to_html(classes="table table-striped")
 
 if __name__ == "__main__":
+
     # If running as script, print scraped data
     print(scrape_all())
-#browser.quit()
